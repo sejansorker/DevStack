f5edc2e8ac1c23c95e2e5b051c19d54d1bd93e8d
sejansorker <sejansorker@gmail.com>
2026-09-13 20:15:34 +0600

    banner text color add

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -8,11 +8,11 @@ const Banner = () => {
            <div className="flex items-center">
              <div className="w-2/3">
                 <h1 className='text-[60px] font-extrabold leading-none pb-5 font-int'>Build Your Ideal
-                  Development Stack </h1>
+                  <span className='bg-gradient-to-r from-orange-500 via-pink-500 to-violet-600 bg-clip-text   text-transparent'> Development Stack</span> </h1>
                   <p className='text-[18px] font-normal pb-10 pr-25'>Explore frontend, backend, database, and tooling options,
                      compare them side by side, and put together the stack that fits your
                      next project.</p>
-                     <button className='p-4 text-[14px] bg-[#F97316] rounded-[8px] mr-4'>Explore Technologies</button>
+                     <button className='p-4 text-[14px] bg-gradient-to-r from-[#ff7a00] to-[#ed3f8f] text-white rounded-[8px] mr-4'>Explore Technologies</button>
                      <button className='py-4 px-11.5 border-1 border-[#E5E7EB] text-[14px]  rounded-[8px] '>Learn More</button>
             </div>
             <div className="w-1/3">
