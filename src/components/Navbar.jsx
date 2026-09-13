5d09021867030df4a85e4fb5f3a84c55757dd1c2
sejansorker <sejansorker@gmail.com>
2026-09-13 17:20:47 +0600

    navbar complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
 import Container from './Container'
-
+import Logo from "../assets/logo.png"
 const Navbar = () => {
   return (
     <div>
         <Container>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam, explicabo et obcaecati minus aliquam ullam, qui praesentium eos nesciunt perspiciatis atque aliquid blanditiis cupiditate facilis! Blanditiis necessitatibus quo exercitationem numquam?
+            <div className="py-6 flex justify-between items-center ">
+                <div className="w-1/4">
+                <div className="">
+                     <img src={Logo} alt="" />
+                </div>
+                </div>
+                <div className="w-2/4">
+                <ul className='flex justify-center items-center gap-x-5'>
+                    <li>Home</li>
+                    <li>Technologies</li>
+                    <li>Projects</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+                </div>
+                <div className="w-1/4">
+                <div className="gap-x-5 flex items-center justify-end">
+                    <button className=''>Sign in</button>
+                    <button className='bg-[#D91B7E] py-2.5 px-5 rounded-[30px]'>Sign up</button>
+                </div>
+                </div>
+            </div>
         </Container>
     </div>
   )
