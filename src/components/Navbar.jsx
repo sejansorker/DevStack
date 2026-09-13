746de3c4d07bde80775618ba6a110d361ed27736
sejansorker <sejansorker@gmail.com>
2026-09-13 16:59:41 +0600

    devstack projeck start

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Container from './Container'
+
+const Navbar = () => {
+  return (
+    <div>
+        <Container>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam, explicabo et obcaecati minus aliquam ullam, qui praesentium eos nesciunt perspiciatis atque aliquid blanditiis cupiditate facilis! Blanditiis necessitatibus quo exercitationem numquam?
+        </Container>
+    </div>
+  )
+}
+
+export default Navbar
